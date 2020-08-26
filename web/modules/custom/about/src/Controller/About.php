2a34ae4726c0f8d2a6eb5741113aba404c94b788
Veoz <VEoz.id@gmail.com>
2020-08-26 18:06:18 +0300

    end about develop

--- web/modules/custom/about/src/Controller/About.php
+++ web/modules/custom/about/src/Controller/About.php
@@ -12,15 +12,15 @@ class About {
     global $base_url;
 
     $about = [];
-//    $query = \Drupal::database()->query("SELECT `slogan`, `text`, `tell`, `mail`, `img` FROM `a_about`");
-//    $result = $query -> fetchAll();
-//
-//    foreach ($result as $row) {
-//      array_push($about, ['slogan' => $row->slogan,'text' => $row->text,'tell' => $row->tell, 'mail' => $row->mail,'img' => $row->img,]);
-//    }
+    $query = \Drupal::database()->query("SELECT `slogan`, `text`, `tell`, `mail`, `img` FROM `a_about`");
+    $result = $query -> fetchAll();
+
+    foreach ($result as $row) {
+      array_push($about, ['slogan' => $row->slogan,'text' => $row->text,'tell' => $row->tell, 'mail' => $row->mail,'img' => $row->img,]);
+    }
 
     $data = array(
-      'title' => 'Welcome to news stream',
+      'title' => 'Its all about us!(c)',
       'about' =>$about
     );
 
